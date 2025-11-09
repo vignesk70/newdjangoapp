15026da4d58684446eee6d0f7ab0cfc1e169bfa5
Vigneswaren MK <vignes_k@yahoo.com>
2025-11-09 15:38:38 +0800

    update settings.py

--- dasan2/dasan2/settings.py
+++ dasan2/dasan2/settings.py
@@ -120,3 +120,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MY_VARIABLE = 'This is a custom setting value'
